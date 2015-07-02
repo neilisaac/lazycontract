91f71eeb449897e2668ef6a20493ad7d598af0d2
Neil Isaac <isaac.neil@gmail.com>
2015-07-02 19:09:55 -0400

    Added alternative name, hidden properties, not none, required, exclude if none, capture deserialization messages

--- lazycontract/contract.py
+++ lazycontract/contract.py
@@ -3,25 +3,37 @@ from __future__ import absolute_import
 import six
 
 
-class LazyContractError(RuntimeError):
+class LazyContractError(Exception):
     pass
 
 
+class LazyContractDeserializationError(LazyContractError):
+    FMT = 'failed to deserialize {}.{} from {} due to: {}'
+
+
 class LazyContractValidationError(LazyContractError):
-    ATTR_FMT = '{}.{} value \'{}\' is not of type {}'
+    INVALID_ATTR_FMT = '{} has no attribute \'{}\''
+    NOT_NONE_FMT = '{} {} must not be None'
+    REQUIRED_FMT = '{}.{} is required'
+    ATTR_TYPE_FMT = '{}.{} value {} is not of type {}'
 
 
 class LazyProperty(object):
 
     _type = type(None)
 
-    def __init__(self, required=False, default=None):
+    _default_name = '(anonymous)'
+
+    def __init__(self, name=None, default=None,
+                 required=False, not_none=False, exclude_if_none=True):
         if required and default is not None:
             raise LazyContractError('default specified for required property')
 
-        self.name = 'anonymous'
+        self.name = name or self._default_name
         self.required = required
         self.default = default
+        self.not_none = not_none
+        self.exclude_if_none = exclude_if_none
 
     def __get__(self, obj, objtype=None):
         value = obj.__dict__.get(self.name, self.default)
@@ -33,11 +45,15 @@ class LazyProperty(object):
         obj.__dict__[self.name] = value
 
     def validate(self, obj):
-        if not isinstance(obj, self._type) and \
-                (obj is not None or self.required):
+        if obj is None and self.not_none:
             raise LazyContractValidationError(
-                    LazyContractValidationError.ATTR_FMT.format(
-                            type(self).__name__, self.name, obj, self._type))
+                    LazyContractValidationError.NOT_NONE_FMT.format(
+                            type(self).__name__, self.name))
+
+        if not isinstance(obj, self._type) and obj is not None:
+            raise LazyContractValidationError(
+                    LazyContractValidationError.ATTR_TYPE_FMT.format(
+                            type(self).__name__, self.name, repr(obj), self._type))
 
     def serialize(self, obj):
         return obj
@@ -53,8 +69,9 @@ class LazyContract(object):
             raise LazyContractError('both _obj and kwargs provided')
 
         self.__properties = dict()
+        self.__mappings = dict()
 
-        self.__discover_properties()
+        self.__discover_properties(_obj or kwargs)
         self.__populate_properties(_obj or kwargs)
 
     def __repr__(self):
@@ -63,20 +80,46 @@ class LazyContract(object):
             ', '.join('{}={}'.format(name, repr(value))
                       for name, _, value in self.__iter_properties()))
 
-    def __discover_properties(self):
+    def __discover_properties(self, obj):
         for name, inst in six.iteritems(type(self).__dict__):
             if isinstance(inst, LazyProperty):
-                inst.name = name
                 self.__properties[name] = inst
+
+                if inst.name == inst._default_name:
+                    inst.name = name
+                else:
+                    self.__mappings[inst.name] = name
+
+                if inst.name not in obj and name not in obj:
+                    if inst.required:
+                        raise LazyContractValidationError(
+                                LazyContractValidationError.REQUIRED_FMT.format(
+                                        type(self).__name__, inst.name))
+
+                    if inst.not_none and inst.default is None:
+                        raise LazyContractValidationError(
+                                LazyContractValidationError.NOT_NONE_FMT.format(
+                                        type(self).__name__, inst.name))
 
     def __populate_properties(self, obj):
         for key, value in six.iteritems(obj):
-            if key in self.__properties:
-                setattr(self, key, self.__properties[key].deserialize(value))
-            else:
+            if key in self.__mappings:
+                key = self.__mappings[key]
+
+            if key not in self.__properties:
                 raise LazyContractValidationError(
-                        """LazyContract '{}' has no attribute '{}'""".format(
+                        LazyContractValidationError.INVALID_ATTR_FMT.format(
                                 type(self).__name__, key))
+
+            if value is not None:
+                try:
+                    value = self.__properties[key].deserialize(value)
+                except Exception as e:
+                    raise LazyContractDeserializationError(
+                            LazyContractDeserializationError.FMT.format(
+                                    type(self).__name__, key, repr(value), e))
+
+            setattr(self, key, value)
 
     def __iter_properties(self):
         for name, prop in six.iteritems(type(self).__dict__):
@@ -84,5 +127,7 @@ class LazyContract(object):
                 yield name, prop, getattr(self, name)
 
     def to_dict(self):
-        return {name: prop.serialize(value) for name, prop, value
-                in self.__iter_properties() if value is not None}
+        return {prop.name: prop.serialize(value) for name, prop, value
+                in self.__iter_properties()
+                if not prop.name.startswith('_') and
+                (value is not None or not prop.exclude_if_none)}
--- lazycontract/test_contract.py
+++ lazycontract/test_contract.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
-from .contract import LazyContract, LazyContractValidationError
+from .contract import (LazyContract, LazyContractValidationError,
+                       LazyContractDeserializationError)
 from .properties import StringProperty, IntegerProperty, FloatProperty
 
 
@@ -48,4 +49,93 @@ def test_invalid_deserialize_attribute():
         exc = e
 
     assert type(exc) == LazyContractValidationError
-    assert 'LazyContract \'TestContract\' has no attribute \'x\'' in str(exc)
+    assert LazyContractValidationError.INVALID_ATTR_FMT.format(
+            'TestContract', 'x') in str(e)
+
+
+def test_required():
+    class TestContract(LazyContract):
+        a = StringProperty(required=True)
+
+    try:
+        TestContract()
+        assert 'LazyContractValidationError expected' == False
+    except LazyContractValidationError as e:
+        assert LazyContractValidationError.REQUIRED_FMT.format(
+                'TestContract', 'a') in str(e)
+
+
+def test_not_none():
+    class TestContract(LazyContract):
+        a = StringProperty(not_none=True)
+
+    try:
+        TestContract()
+        assert 'LazyContractValidationError expected' == False
+    except LazyContractValidationError as e:
+        assert LazyContractValidationError.NOT_NONE_FMT.format(
+                'TestContract', 'a') in str(e)
+
+    try:
+        TestContract(a=None)
+        assert 'LazyContractValidationError expected' == False
+    except LazyContractValidationError as e:
+        assert LazyContractValidationError.NOT_NONE_FMT.format(
+                'StringProperty', 'a') in str(e)
+
+    t = TestContract(a='foobar')
+    try:
+        t.a = None
+        assert 'LazyContractValidationError expected' == False
+    except LazyContractValidationError as e:
+        assert LazyContractValidationError.NOT_NONE_FMT.format(
+                'StringProperty', 'a') in str(e)
+
+
+def test_hidden_property():
+    class TestContract(LazyContract):
+        _a = StringProperty()
+
+    assert TestContract(_a='foobar').to_dict() == dict()
+
+
+def test_exclude_if_none():
+    class TestContract(LazyContract):
+        a = StringProperty(exclude_if_none=False)
+
+    assert TestContract().to_dict() == dict(a=None)
+
+
+def test_deserialization_error():
+    class TestContract(LazyContract):
+        a = IntegerProperty()
+
+    reason = None
+    try:
+        int('3.2')
+    except Exception as reason:
+        pass
+
+    try:
+        TestContract(a='3.2')
+        assert 'expected LazyContractDeserializationError' == False
+    except LazyContractDeserializationError as e:
+        assert LazyContractDeserializationError.FMT.format(
+                'TestContract', 'a', repr('3.2'), reason) in str(e)
+
+
+def test_serialization_name():
+    class TestContract(LazyContract):
+        a = StringProperty(name='b')
+
+    t = TestContract(a='a1')
+    assert t.to_dict() == dict(b='a1')
+
+    t.a = 'a2'
+    assert t.to_dict() == dict(b='a2')
+
+    t = TestContract(b='b1')
+    assert t.to_dict() == dict(b='b1')
+
+    t.b = 'b2'
+    assert t.to_dict() == dict(b='b2')

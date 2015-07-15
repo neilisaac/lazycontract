ae880af184da3aa1af04cc0705f9ad9cc4a7bf79
Neil Isaac <isaac.neil@gmail.com>
2015-07-14 21:30:55 -0400

    Update setup before pushing test version to pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,21 @@
 from setuptools import setup
 
 setup(name='lazycontract',
-      version='1.0',
-      description='Python library to define declarative contracts for serialization and (lazy) deserialization',
+      version='0.9',
+      description='Python library to define declarative contracts for serialization and deserialization',
       author='Neil Isaac',
       author_email='isaac.neil@gmail.com',
+      license='MIT',
       url='https://github.com/neilisaac/lazycontract',
       packages=['lazycontract'],
       install_requires=['six'],
       tests_require=['pytest', 'tox'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries']
       )
